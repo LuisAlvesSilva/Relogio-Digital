c1d71a53ebfa424ad14ebd9a449132766fabe03f
Luis Fernando Alves da Silva <121822005+LuisAlvesSilva@users.noreply.github.com>
2024-05-15 11:50:48 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,26 @@ const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
 
+const dia = document.getElementById('dia')
+const mes = document.getElementById('mes')
+const ano = document.getElementById('ano')
+
 const relogio = setInterval(function time() {
     let dateToday = new Date()
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
+    let d = dateToday.getDay();
+    let m = dateToday.getMonth();
+    let a = dateToday.getFullYear();
+
     horas.textContent = fixTime(hr);
     minutos.textContent = fixTime(min);
     segundos.textContent = fixTime(sec);
+    dia.textContent = fixTime(d);
+    mes.textContent = fixTime(m);
+    ano.textContent = fixTime(a);
 
     function fixTime(time){
         return time < 10 ? '0' + time : time
